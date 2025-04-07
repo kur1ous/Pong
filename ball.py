368a12bbbc45ad6b06a2656fa87112400be16ab2
kurious <108778179+kur1ous@users.noreply.github.com>
2025-04-07 14:07:14 -0400

    revamped ball creation

--- ball.py
+++ ball.py
@@ -3,34 +3,30 @@ from settings import *
 import random
 
 class Ball(pygame.sprite.Sprite):
-    def __init__(self, x, y, radius, x_velocity, y_velocity, groups):
+    def __init__(self, x, y, radius, groups):
         super().__init__(groups)
         self.x = x 
         self.y = y
         self.radius = radius
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
-        self.colour = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 
         self.image = pygame.Surface((2*radius, 2*radius))
+        self.image.set_colorkey((0,0,0))
+        self.colour = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        pygame.draw.circle(self.image, self.colour, (radius,radius), radius)
 
+        self.rect = self.image.get_frect(center=(x,y))
+        self.x_velocity = 1
+        self.y_velocity = 1
 
+    def update(self):
+        self.rect.x += self.x_velocity
+        self.rect.y += self.y_velocity
 
-    def draw(self):
-        pygame.draw.circle(screen,(self.colour), (self.x, self.y), self.radius)
-
-
-    def move(self):
-        self.x = self.x+self.x_velocity
-        self.y = self.y+self.y_velocity
-
-    def wall_collision(self):
-        if self.x >= SCREEN_WIDTH-self.radius:
+        if self.rect.right >= SCREEN_WIDTH and self.x_velocity >= 0:
             self.x_velocity *= -1
-        if self.x <= 0+self.radius:
+        if self.rect.left <= 0 and self.x_velocity <= 0:
             self.x_velocity *= -1
-        if self.y >= SCREEN_HEIGHT-self.radius:
+        if self.rect.bottom >= SCREEN_HEIGHT and self.y_velocity >= 0:
             self.y_velocity *= -1
-        if self.y <= 0+self.radius:
+        if self.rect.top <= 0 and self.y_velocity <= 0:
             self.y_velocity *= -1
-        
